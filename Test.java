c2835a0af37670db8942372457c669a61d564a9c
m-zasky <mmuhammadzasky@gmail.com>
2025-10-02 13:48:33 +0700

    zasky

--- Test.java
+++ Test.java
@@ -1,5 +1,6 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("hi, ini baris pertama");
+        System.out.println("tes Penerima MUHAMMAD ZASKY ARYO MAHADANI" );
     }
 }
